bb0b647cd569fd3a72910ada183d585254d46bb2
Viraj Shah <viraj.v.shah03@gmail.com>
2024-04-14 02:42:11 +0530

    feat: fixed gui

--- app.py
+++ app.py
@@ -15,62 +15,41 @@ class Application:
         self.is_running = True
         master.title("REMDR")
         master.configure(bg="lightgrey")
-        master.geometry("1280x900")
         master.resizable(False, False)
         # Create three frames for the three columns
+        master.geometry("1280x530")
         self.left_frame = ctk.CTkFrame(master)
         self.left_frame.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
-
-        self.center_frame = ctk.CTkFrame(master)  # Add this line
-        self.center_frame.grid(
-            row=0, column=1, padx=10, pady=10, sticky="nsew"
-        )  # Add this line
 
         self.right_frame = ctk.CTkFrame(master)  # Add this line
         self.right_frame.grid(
             row=0, column=2, padx=10, pady=10, sticky="nsew"
         )  # Add this line
 
-        self.center_placeholder = ctk.CTkLabel(self.center_frame, text="")
-        self.center_placeholder.grid(row=1, column=0, sticky="nsew", padx=250, pady=10)
-
-        self.center_placeholder_bottom = ctk.CTkLabel(self.center_frame, text="")
-        self.center_placeholder_bottom.grid(row=2, column=0, sticky="nsew", padx=250)
-
         self.right_placeholder = ctk.CTkLabel(
             self.right_frame, text="Other stuff goes here"
         )
         self.right_placeholder.grid(row=1, column=0, sticky="nsew")
 
-        self.camera_feed_label = ctk.CTkLabel(self.center_frame, text="")
-        self.camera_feed_label.grid(row=0, column=0, sticky="nsew")
-
-        # Configure the grid to expand as the window size changes
-        master.grid_rowconfigure(0, weight=1)
-        master.grid_columnconfigure(0, weight=1)
-        master.grid_columnconfigure(1, weight=1)
-        master.grid_columnconfigure(2, weight=1)
-
-        # Add the widgets to the left frame
         self.weights_label = ctk.CTkLabel(self.left_frame, text="Weights:")
         self.weights_label.grid(row=0, column=0, sticky="nsew", padx=(10, 0), pady=15)
         self.weights_entry = ctk.CTkEntry(self.left_frame)
-        self.weights_entry.grid(row=0, column=1, sticky="nsew", padx=(10, 0), pady=15)
+        self.weights_entry.grid(row=0, column=1, sticky="nsew", padx=(10, 10), pady=15)
 
         self.source_label = ctk.CTkLabel(self.left_frame, text="Source:")
         self.source_label.grid(row=0, column=2, sticky="nsew", padx=(10, 0), pady=15)
         self.source_entry = ctk.CTkEntry(self.left_frame)
-        self.source_entry.grid(row=0, column=3, sticky="nsew", padx=(10, 0), pady=15)
+        self.source_entry.grid(row=0, column=3, sticky="nsew", padx=(10, 10), pady=15)
 
         self.data_label = ctk.CTkLabel(self.left_frame, text="Data:")
         self.data_label.grid(row=1, column=0, sticky="nsew", padx=(10, 0), pady=15)
         self.data_entry = ctk.CTkEntry(self.left_frame)
-        self.data_entry.grid(row=1, column=1, sticky="nsew", padx=(10, 0), pady=15)
+        self.data_entry.grid(row=1, column=1, sticky="nsew", padx=(10, 10), pady=15)
 
         self.imgsz_label = ctk.CTkLabel(self.left_frame, text="Image Size:")
         self.imgsz_label.grid(row=1, column=2, sticky="nsew", padx=(10, 0), pady=15)
         self.imgsz_entry = ctk.CTkEntry(self.left_frame)
-        self.imgsz_entry.grid(row=1, column=3, sticky="nsew", padx=(10, 0), pady=15)
+        self.imgsz_entry.grid(row=1, column=3, sticky="nsew", padx=(10, 10), pady=15)
 
         self.conf_thres_label = ctk.CTkLabel(
             self.left_frame, text="Confidence Threshold:"
@@ -80,42 +59,42 @@ class Application:
         )
         self.conf_thres_entry = ctk.CTkEntry(self.left_frame)
         self.conf_thres_entry.grid(
-            row=2, column=1, sticky="nsew", padx=(10, 0), pady=15
+            row=2, column=1, sticky="nsew", padx=(10, 10), pady=15
         )
 
         self.iou_thres_label = ctk.CTkLabel(self.left_frame, text="NMS IOU Threshold:")
-        self.iou_thres_label.grid(row=2, column=0, sticky="nsew", padx=(10, 0), pady=15)
+        self.iou_thres_label.grid(row=2, column=2, sticky="nsew", padx=(10, 0), pady=15)
         self.iou_thres_entry = ctk.CTkEntry(self.left_frame)
         self.iou_thres_entry.grid(
-            row=2, column=1, sticky="nsew", padx=(10, 0), pady=15
+            row=2, column=3, sticky="nsew", padx=(10, 10), pady=15
         )
 
         self.max_det_label = ctk.CTkLabel(self.left_frame, text="Max Detections:")
         self.max_det_label.grid(row=3, column=0, sticky="nsew", padx=(10, 0), pady=15)
         self.max_det_entry = ctk.CTkEntry(self.left_frame)
-        self.max_det_entry.grid(row=3, column=1, sticky="nsew", padx=(10, 0), pady=15)
+        self.max_det_entry.grid(row=3, column=1, sticky="nsew", padx=(10, 10), pady=15)
 
         self.device_label = ctk.CTkLabel(self.left_frame, text="Device:")
-        self.device_label.grid(row=3, column=0, sticky="nsew", padx=(10, 0), pady=15)
+        self.device_label.grid(row=3, column=2, sticky="nsew", padx=(10, 0), pady=15)
         self.device_entry = ctk.CTkEntry(self.left_frame)
-        self.device_entry.grid(row=3, column=1, sticky="nsew", padx=(10, 0), pady=15)
+        self.device_entry.grid(row=3, column=3, sticky="nsew", padx=(10, 10), pady=15)
 
         self.save_txt_label = ctk.CTkLabel(
             self.left_frame, text="Save Results to *.txt:"
         )
         self.save_txt_label.grid(row=4, column=0, sticky="nsew", padx=(10, 0), pady=15)
         self.save_txt_entry = ctk.CTkEntry(self.left_frame)
-        self.save_txt_entry.grid(row=4, column=1, sticky="nsew", padx=(10, 0), pady=15)
+        self.save_txt_entry.grid(row=4, column=1, sticky="nsew", padx=(10, 10), pady=15)
 
         self.save_conf_label = ctk.CTkLabel(
             self.left_frame, text="Save Confidences in --save-txt Labels:"
         )
         self.save_conf_label.grid(
-            row=5, column=0, sticky="nsew", padx=(10, 0), pady=15
+            row=4, column=2, sticky="nsew", padx=(10, 0), pady=15
         )
         self.save_conf_entry = ctk.CTkEntry(self.left_frame)
         self.save_conf_entry.grid(
-            row=5, column=1, sticky="nsew", padx=(10, 0), pady=15
+            row=4, column=3, sticky="nsew", padx=(10, 10), pady=15
         )
 
         self.save_crop_label = ctk.CTkLabel(
@@ -126,38 +105,38 @@ class Application:
         )
         self.save_crop_entry = ctk.CTkEntry(self.left_frame)
         self.save_crop_entry.grid(
-            row=5, column=1, sticky="nsew", padx=(10, 0), pady=15
+            row=5, column=1, sticky="nsew", padx=(10, 10), pady=15
         )
 
         self.nosave_label = ctk.CTkLabel(
             self.left_frame, text="Do Not Save Images/Videos:"
         )
-        self.nosave_label.grid(row=6, column=0, sticky="nsew", padx=(10, 0), pady=15)
+        self.nosave_label.grid(row=5, column=2, sticky="nsew", padx=(10, 0), pady=15)
         self.nosave_entry = ctk.CTkEntry(self.left_frame)
-        self.nosave_entry.grid(row=6, column=1, sticky="nsew", padx=(10, 0), pady=15)
+        self.nosave_entry.grid(row=5, column=3, sticky="nsew", padx=(10, 10), pady=15)
 
         self.classes_label = ctk.CTkLabel(
             self.left_frame, text="Filter by Class (--class 0, or --class 0 2 3):"
         )
         self.classes_label.grid(row=6, column=0, sticky="nsew", padx=(10, 0), pady=15)
         self.classes_entry = ctk.CTkEntry(self.left_frame)
-        self.classes_entry.grid(row=6, column=1, sticky="nsew", padx=(10, 0), pady=15)
+        self.classes_entry.grid(row=6, column=1, sticky="nsew", padx=(10, 10), pady=15)
 
         self.agnostic_nms_label = ctk.CTkLabel(
             self.left_frame, text="Class-Agnostic NMS:"
         )
         self.agnostic_nms_label.grid(
-            row=7, column=0, sticky="nsew", padx=(10, 0), pady=15
+            row=6, column=2, sticky="nsew", padx=(10, 0), pady=15
         )
         self.agnostic_nms_entry = ctk.CTkEntry(self.left_frame)
         self.agnostic_nms_entry.grid(
-            row=7, column=1, sticky="nsew", padx=(10, 0), pady=15
+            row=6, column=3, sticky="nsew", padx=(10, 10), pady=15
         )
 
         self.augment_label = ctk.CTkLabel(self.left_frame, text="Augmented Inference:")
         self.augment_label.grid(row=7, column=0, sticky="nsew", padx=(10, 0), pady=15)
         self.augment_entry = ctk.CTkEntry(self.left_frame)
-        self.augment_entry.grid(row=7, column=1, sticky="nsew", padx=(10, 0), pady=15)
+        self.augment_entry.grid(row=7, column=1, sticky="nsew", padx=(10, 10), pady=15)
 
         self.run_button = ctk.CTkButton(
             self.left_frame,
@@ -165,12 +144,12 @@ class Application:
             command=self.run_detection,
             corner_radius=10,
         )
-        self.run_button.grid(row=16, column=0, columnspan=2, pady=10)
+        self.run_button.grid(row=8, column=0, columnspan=2, pady=10)
 
         self.exit_button = ctk.CTkButton(
             self.left_frame, text="Exit", command=self.exit_app, corner_radius=10
         )
-        self.exit_button.grid(row=17, column=0, columnspan=2, pady=10)
+        self.exit_button.grid(row=8, column=1, columnspan=2, pady=10)
 
     def run_detection(self):
         weights = self.weights_entry.get()
@@ -181,23 +160,9 @@ class Application:
                 break
             cv2.imshow(p, im0)
 
-    # def run_detection(self):
-    #     weights = self.weights_entry.get()
-    #     source = self.source_entry.get()
-
-    #     for cv2,p,im0 in run():
-    #         cv2.imshow(p,im0)
-    #         cv2image = cv2.cvtColor(im0, cv2.COLOR_BGR2RGBA)
-    #         img = Image.fromarray(cv2image)
-    #         imgtk = ImageTk.PhotoImage(image=img)
-    #         self.camera_feed_label['Image'] = imgtk
-    #         self.camera_feed_label.imgtk = imgtk
-    #         self.camera_feed_label.configure(image=imgtk)
-
     def exit_app(self):
         os._exit(0)
-
-
+        
 if __name__ == "__main__":
     root = ctk.CTk()
     app = Application(root)
